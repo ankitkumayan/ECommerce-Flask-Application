e0d9fddd18a423500d67b29904d762475287e10b
LCF2319 <c0921273@mylambton.ca>
2024-10-18 01:08:54 -0400

    Your commit message

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-
 from flask import Flask, render_template
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+MONGODB_USERNAME = os.getenv('ankit26114')
+MONGODB_PASSWORD = os.getenv('123Ankit')
 
 app = Flask(__name__)
 
-# MongoDB Atlas connection URI
-client = MongoClient('mongodb+srv://ankit26114:123Ankit@cluster0.vgyrk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+client = MongoClient(f'mongodb+srv://ankit26114:123Ankit@cluster0.vgyrk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
-# Connect to the database and collection
 db = client['shop_db']
 products_collection = db['products']
 
@@ -17,7 +22,6 @@ def home():
 
 @app.route('/products')
 def products():
-    # Fetch product data from MongoDB
     products = products_collection.find()
     return render_template('products.html', products=products)
 
