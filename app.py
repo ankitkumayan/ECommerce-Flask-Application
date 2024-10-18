c9594ec526920ead02f70869f7cb3809046401e4
LCF2319 <c0921273@mylambton.ca>
2024-10-18 03:05:20 -0400

    env changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
+# Load environment variables from .env
 load_dotenv()
 
 MONGODB_USERNAME = os.getenv('ankit26114')
